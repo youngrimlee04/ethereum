36476e9f3727bd32b1f9c59dd175b6fa5d4150fb
youngrimlee04 <youngrimlee04@github.com>
2019-09-06 11:33:06 +0900

    Update mongo_insert_member.js

--- routes/mongo_insert_member.js
+++ routes/mongo_insert_member.js
@@ -4,17 +4,49 @@
 // 항목 : 아이디, 암호, 이름, 주소, 나이
 // db명 : i t e m   
 // collection : member 
-// 5명의 회원 추가
+// 5명의 회원 추가 
 
 var express = require('express');
 var router = express.Router();
 
+var Web3 = require('web3');
+
 // npm install mongodb --save
 var MongoClient = require("mongodb").MongoClient;
 
+var w3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+
+
 // 127.0.0.1:3000/member/insert
 router.get('/insert', function(req, res, next) {
-    res.render("member"); // view인 member.ejs로 보내기
+    MongoClient.connect('mongodb://localhost:27017/item', function(err, dbconn){
+        if(err) {
+          console.log('error', err);
+        }
+        else{
+          var collection = dbconn.db("item").collection('abi');
+          
+          //
+          collection.find({}).toArray(function (err, docs) {
+            
+            console.log(docs[0].CA)
+            console.log(docs[0].ABI)
+            var ABI = docs[0].ABI
+            var CA = docs[0].CA
+
+            // web3_cont_deploy에서 가져와서 찍어보기
+            var get_contract = new w3.eth.Contract(ABI, CA);
+
+            get_contract.methods.getName().call().then(data => {
+                console.log('getName : ', data);
+            });
+
+            res.render("member", {CA:CA}); // CA값 찍어보기 
+          });
+        }
+    });
+
+    
 });
 
 
