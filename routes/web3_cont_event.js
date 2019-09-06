873724c0990c5c5022f97cf4ca79b0e280460270
youngrimlee04 <youngrimlee04@github.com>
2019-09-06 11:32:43 +0900

    Update web3_cont_event.js

--- routes/web3_cont_event.js
+++ routes/web3_cont_event.js
@@ -5,10 +5,74 @@ var router = express.Router();
 var Web3 = require('web3');
 
 const ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_var1",
+				"type": "string"
+			}
+		],
+		"name": "setString",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_var1",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "val1",
+				"type": "string"
+			}
+		],
+		"name": "E_setString",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getString",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "var1",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
 	}
 ]
 
-const CA = '';
+const CA = '0xc32cc5019ef11448567aab00fc48348b12d56ade';
 
 var w3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
 var ws3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'))
